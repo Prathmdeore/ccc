d2a0cf681e2763631e9ae21f64e51e4e54465aa7
Prathmdeore <144782372+Prathmdeore@users.noreply.github.com>
2023-09-12 14:21:34 +0530

    Create data.cpp

---
+++ data.cpp
@@ -0,0 +1,20 @@
+#include<iostram.h>'
+#include<string.h>
+using namespace std ;
+int main()
+{
+  string s;
+  int roll;
+  string subname;
+  cout<<"enter your name"<<endl;
+  cin>>s;
+  cout<<"enter your roll number"<<endl;
+  cin>>roll;
+  cout<<"enter the name of your subject"<<endl;
+  cin>>subname;
+  cout<<"displaying your details"<<endl;
+  cout<<s<<endl;
+  cout<<roll<<endl;
+  cout<<subname<<endl;
+  
+}
